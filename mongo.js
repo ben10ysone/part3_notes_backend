5e973ffdf66f5d4df8f58d1b3bf0b0a1cc6ad3c8
Arnav Goel <2828.arnav@gmail.com>
2025-07-25 09:09:41 +0000

    completed part4b

--- mongo.js
+++ mongo.js
@@ -7,30 +7,42 @@ const mongoose = require('mongoose')
 
 // const password = process.argv[2]
 
-const url = 'mongodb+srv://arnavgg458:qlCQEnDeGdJ6fvES@cluster0.fknuy9s.mongodb.net/noteApp?retryWrites=true&w=majority&appName=cluster0'
+const url = 'mongodb+srv://arnavgg458:qlCQEnDeGdJ6fvES@cluster0.fknuy9s.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=cluster0'
 
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
   important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
-//   content: 'is mongoDB really good?',
+//   content: 'Learn to build anything',
 //   important: true,
 // })
 
-// note.save().then(result => {
+// note.save().then(() => {
 //   console.log('note saved!')
 //   mongoose.connection.close()
 // })
 
-Note.find({ important:false }).then(result => {
+Note.find({}).then(result => {
   result.forEach(note => {
     console.log(note)
   })
